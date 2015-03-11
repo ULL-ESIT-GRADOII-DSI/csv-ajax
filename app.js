9189b7ab7ff87465a471766593cb80043f754db4
alu0100764666 <alu0100764666@ull.edu.es>
2015-03-11 15:29:44 +0000

    Creado las views

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
 
 app.set('port', (process.env.PORT || 5000));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (request, response) {
