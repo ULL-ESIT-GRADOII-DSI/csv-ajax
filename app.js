c6356d1e8b24d4b50dc4a7a67baf2e269a2a2225
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-04-05 15:48:56 +0100

    separated calculate n a module

--- app.js
+++ app.js
@@ -16,91 +16,15 @@ app.get('/', function (request, response) {
     response.render('index', { title: 'CSV Analyzer' });
 });
 
-var regexp = /"((?:[^"\\]|\\.)*)"|([^,\s]+)|,\s*(?=,|$)|^\s*,/g
-var calculate = function(original) {
-    var lines = original.split(/\n+\s*/);
-    var commonLength = lines[0].match(regexp).length;
-    var r = [];
-    var removeQuotes = function(field) {
-      var removecomma = field.replace(/,\s*$/, '');
-      var remove1stquote = removecomma.replace(/^\s*"/, '');
-      var removelastquote = remove1stquote.replace(/"\s*$/, '');
-      var removeescapedquotes = removelastquote.replace(/\\"/, '"');
-      return removeescapedquotes;
-    };
-
-    for (var t in lines) {
-      var temp = lines[t];
-      var m = temp.match(regexp);
-      var result = [];
-      var error = false;
-
-      // skip empty lines and comments
-      if (temp.match(/(^\s*$)|(^#.*)/)) continue; 
-      if (m) {
-        result = m.map(removeQuotes);
-        error = (commonLength != m.length);
-        var rowclass = error? 'error' : 'legal';
-        r.push({ items: result, typ: rowclass });
-      }
-      else {
-        var errmsg = 'La fila "' + temp + '" no es un valor de CSV permitido.';
-        r.push({value: errmsg.split("").splice(commonLength), rowClass: 'error'});
-      }
-    }
-    return r;
-  };
+var calculate = require('calculate');
 
 app.get('/separateCSV', function (request, response) {
-
-    var result;
     var original = request.query.input
-    var temp = original;
-    var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
-    var lines = temp.split(/\n+\s*/);
-    var commonLength = NaN;
-    var row;
     var rows = calculate(original);
-   
-    /*
-    for (var t in lines) {
-        var temp = lines[t];
-        var m = temp.match(regexp);
-        var result = [];
-        var error = false;
-
-        if (m) {
-            if (commonLength && (commonLength != m.length)) {
-                //alert('ERROR! row <'+temp+'> has '+m.length+' items!');
-                error = true;
-            }
-            else {
-                commonLength = m.length;
-                error = false;
-            }
-            for (var i in m) {
-                var removecomma = m[i].replace(/,\s*$/, '');
-                var remove1stquote = removecomma.replace(/^\s*"/, '');
-                var removelastquote = remove1stquote.replace(/"\s*$/, '');
-                var removeescapedquotes = removelastquote.replace(/\\"/, '"');
-                result.push(removeescapedquotes);
-            }
-            var tr = error ? 'error' : 'legal';
-            row = new Object();
-            row.type = tr;
-            row.items = result;
-            rows.push(row);
-        }
-        else {
-            console.log('ERROR! row <' + temp + '> does not look as legal CSV');
-            error = true;
-        }
-    }
-    */
+    
     response.send({ "rows": rows });
 });
 
 app.listen(app.get('port'), function () {
     console.log("Node app is running at localhost:" + app.get('port'));
-   
 });
